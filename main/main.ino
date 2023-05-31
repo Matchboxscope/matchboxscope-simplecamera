18927be705fd7ed8ee94b9ad1b664ff5847bbdee
beniroquai <bene.d@gmx.de>
2023-05-31 14:07:07 +0200

    updating autofocus streaming code for imswitch

--- main/main.ino
+++ main/main.ino
@@ -3,25 +3,29 @@
 
 #define BAUD_RATE 2000000
 
-#define PWDN_GPIO_NUM     -1
-#define RESET_GPIO_NUM    -1
-#define XCLK_GPIO_NUM     10
-#define SIOD_GPIO_NUM     40
-#define SIOC_GPIO_NUM     39
+#define PWDN_GPIO_NUM -1
+#define RESET_GPIO_NUM -1
+#define XCLK_GPIO_NUM 10
+#define SIOD_GPIO_NUM 40
+#define SIOC_GPIO_NUM 39
 
-#define Y9_GPIO_NUM       48
-#define Y8_GPIO_NUM       11
-#define Y7_GPIO_NUM       12
-#define Y6_GPIO_NUM       14
-#define Y5_GPIO_NUM       16
-#define Y4_GPIO_NUM       18
-#define Y3_GPIO_NUM       17
-#define Y2_GPIO_NUM       15
-#define VSYNC_GPIO_NUM    38
-#define HREF_GPIO_NUM     47
-#define PCLK_GPIO_NUM     13
+#define Y9_GPIO_NUM 48
+#define Y8_GPIO_NUM 11
+#define Y7_GPIO_NUM 12
+#define Y6_GPIO_NUM 14
+#define Y5_GPIO_NUM 16
+#define Y4_GPIO_NUM 18
+#define Y3_GPIO_NUM 17
+#define Y2_GPIO_NUM 15
+#define VSYNC_GPIO_NUM 38
+#define HREF_GPIO_NUM 47
+#define PCLK_GPIO_NUM 13
 
-#define LED_GPIO_NUM      21
+#define LED_GPIO_NUM 21
+
+void grabImage();
+void cameraInit();
+
 
 void setup()
 {
@@ -30,11 +34,97 @@ void setup()
   cameraInit();
 }
 
-void loop() {
-  grabImage();
+bool isCROP = false;
+int Nx = 320;
+int Ny = 240;
+int Nroi = 50;
+int x = 320 / 2;
+int y = 240 / 2;
+bool isStreaming = true;
+
+void crop_image(camera_fb_t *fb, unsigned short cropLeft, unsigned short cropRight, unsigned short cropTop, unsigned short cropBottom)
+{
+  unsigned int maxTopIndex = cropTop * fb->width * 2;
+  unsigned int minBottomIndex = ((fb->width * fb->height) - (cropBottom * fb->width)) * 2;
+  unsigned short maxX = fb->width - cropRight; // In pixels
+  unsigned short newWidth = fb->width - cropLeft - cropRight;
+  unsigned short newHeight = fb->height - cropTop - cropBottom;
+  size_t newJpgSize = newWidth * newHeight * 2;
+
+  unsigned int writeIndex = 0;
+  // Loop over all bytes
+  for (int i = 0; i < fb->len; i += 2)
+  {
+    // Calculate current X, Y pixel position
+    int x = (i / 2) % fb->width;
+
+    // Crop from the top
+    if (i < maxTopIndex)
+    {
+      continue;
+    }
+
+    // Crop from the bottom
+    if (i > minBottomIndex)
+    {
+      continue;
+    }
+
+    // Crop from the left
+    if (x <= cropLeft)
+    {
+      continue;
+    }
+
+    // Crop from the right
+    if (x > maxX)
+    {
+      continue;
+    }
+
+    // If we get here, keep the pixels
+    fb->buf[writeIndex++] = fb->buf[i];
+    fb->buf[writeIndex++] = fb->buf[i + 1];
+  }
+
+  // Set the new dimensions of the framebuffer for further use.
+  fb->width = newWidth;
+  fb->height = newHeight;
+  fb->len = newJpgSize;
 }
 
-void cameraInit() {
+void loop()
+{
+  // Check for incoming serial commands
+  if (Serial.available() > 0)
+  {
+    String command = Serial.readStringUntil('\n'); // Read the incoming command until a newline character is encountered
+
+    // Parse the received command into x and y coordinates
+    int delimiterIndex = command.indexOf(','); // Find the index of the comma delimiter
+    if (delimiterIndex != -1)
+    {
+      String xString = command.substring(0, delimiterIndex);  // Extract the x coordinate substring
+      String yString = command.substring(delimiterIndex + 1); // Extract the y coordinate substring
+
+      x = xString.toInt(); // Convert the x coordinate string to an integer
+      y = yString.toInt(); // Convert the y coordinate string to an integer
+
+      // Do something with the x and y coordinates
+      // For example, you can print them:
+      Serial.print("Received coordinates: ");
+      Serial.print("x = ");
+      Serial.print(x);
+      Serial.print(", y = ");
+      Serial.println(y);
+    }
+  }
+  if (isStreaming)
+    grabImage();
+}
+
+void cameraInit()
+{
 
   camera_config_t config;
   config.ledc_channel = LEDC_CHANNEL_0;
@@ -56,35 +146,87 @@ void cameraInit() {
   config.pin_pwdn = PWDN_GPIO_NUM;
   config.pin_reset = RESET_GPIO_NUM;
   config.xclk_freq_hz = 20000000;
-    config.frame_size = FRAMESIZE_QVGA;
-  config.pixel_format = PIXFORMAT_JPEG; // for streaming
-  //config.pixel_format = PIXFORMAT_RGB565; // for face detection/recognition
+  // config.frame_size = FRAMESIZE_QVGA;
+  // config.pixel_format = PIXFORMAT_JPEG; // for streaming
+  // config.pixel_format = PIXFORMAT_RGB565; // for face detection/recognition
   config.grab_mode = CAMERA_GRAB_WHEN_EMPTY;
   config.fb_location = CAMERA_FB_IN_PSRAM;
   config.jpeg_quality = 12;
-  config.fb_count = 2;
+  if (isCROP)
+  {
+    config.pixel_format = PIXFORMAT_RGB565;
+    config.frame_size = FRAMESIZE_SXGA;
+    config.fb_count = 2;
+  }
+  else
+  {
+    config.pixel_format = PIXFORMAT_JPEG;
+    config.frame_size = FRAMESIZE_VGA; // for streaming}
+
+    config.fb_count = 1;
+  }
 
   esp_err_t err = esp_camera_init(&config);
-  if (err != ESP_OK) {
+  if (err != ESP_OK)
+  {
     return;
   }
 
-  sensor_t * s = esp_camera_sensor_get();
+  sensor_t *s = esp_camera_sensor_get();
   s->set_hmirror(s, 1);
   s->set_vflip(s, 1);
 }
+void grabImage()
+{
 
-void grabImage() {
+  camera_fb_t *fb = esp_camera_fb_get();
+  if (isCROP)
+  {
 
-  camera_fb_t* fb = esp_camera_fb_get();
-  if (!fb || fb->format != PIXFORMAT_JPEG) {
-  } else {
-    delay(40);
+    // Crop image (frame buffer, cropLeft, cropRight, cropTop, cropBottom)
+    unsigned short cropLeft = x - Nroi / 2;
+    unsigned short cropRight = x + Nroi / 2;
+    unsigned short cropTop = y - Nroi / 2;
+    unsigned short cropBottom = y + Nroi / 2;
 
-    String encoded = base64::encode(fb->buf, fb->len);
-    Serial.write(encoded.c_str(), encoded.length());
-    Serial.println();
-    
+    crop_image(fb, 550, 450, 100, 190);
+    // crop_image(fb, cropLeft, cropRight, cropTop, cropBottom);
+    //  Create a buffer for the JPG in psram
+    uint8_t *jpg_buf = (uint8_t *)heap_caps_malloc(200000, MALLOC_CAP_SPIRAM | MALLOC_CAP_8BIT);
+
+    if (jpg_buf == NULL)
+    {
+      printf("Malloc failed to allocate buffer for JPG.\n");
+    }
+    else
+    {
+      size_t jpg_size = 0;
+
+      // Convert the RAW image into JPG
+      // The parameter "31" is the JPG quality. Higher is better.
+      fmt2jpg(fb->buf, fb->len, fb->width, fb->height, fb->format, 31, &jpg_buf, &jpg_size);
+      printf("Converted JPG size: %d bytes \n", jpg_size);
+      String encoded = base64::encode(jpg_buf, jpg_size);
+      Serial.write(encoded.c_str(), encoded.length());
+      Serial.println();
+    }
   }
-  esp_camera_fb_return(fb);
+  else
+  {
+
+    if (!fb || fb->format != PIXFORMAT_JPEG)
+    {
+      Serial.println("Failed to capture image");
+    }
+    else
+    {
+      delay(40);
+
+      String encoded = base64::encode(fb->buf, fb->len);
+      Serial.write(encoded.c_str(), encoded.length());
+      Serial.println();
+    }
+
+    esp_camera_fb_return(fb);
+  }
 }
