99142a2cb16c96671b6f11aecf46cd80e73823a3
beniroquai <bene.d@gmx.de>
2023-06-06 13:00:06 +0200

    Adding flush to serial output

--- main/main.ino
+++ main/main.ino
@@ -1,14 +1,7 @@
 #include "esp_camera.h"
+#include <base64.h>
 
-#define FRAME_SIZE FRAMESIZE_QVGA
-#define WIDTH 320
-#define HEIGHT 240
-#define BLOCK_SIZE 2
-#define W (WIDTH / BLOCK_SIZE)
-#define H (HEIGHT / BLOCK_SIZE)
-#define BLOCK_DIFF_THRESHOLD 0.2
-#define IMAGE_DIFF_THRESHOLD 0.1
-#define DEBUG 1
+#define BAUD_RATE 2000000
 
 #define PWDN_GPIO_NUM -1
 #define RESET_GPIO_NUM -1
@@ -30,291 +23,197 @@
 
 #define LED_GPIO_NUM 21
 
-uint16_t proc_frame[H][W] = {0};
-uint16_t current_frame[H][W] = {0};
+void grabImage();
+void cameraInit();
 
-bool setup_camera(framesize_t);
-bool capture_still();
-void convolve_gaussian();
-void find_max_pix();
-void print_frame(uint16_t frame[H][W]);
-int max_x = 0;
-int max_y = 0;
 
-/**
-
-*/
 void setup()
 {
-    Serial.begin(1000000);
-    cameraInit();
+  Serial.begin(BAUD_RATE);
 
-    // flush serial
-      while (Serial.available() > 0) {
-    char c = Serial.read();
-  }
+  cameraInit();
 }
 
-/**
+bool isCROP = false;
+int Nx = 320;
+int Ny = 240;
+int Nroi = 50;
+int x = 320 / 2;
+int y = 240 / 2;
+bool isStreaming = true;
 
-*/
-void loop()
+void crop_image(camera_fb_t *fb, unsigned short cropLeft, unsigned short cropRight, unsigned short cropTop, unsigned short cropBottom)
 {
+  unsigned int maxTopIndex = cropTop * fb->width * 2;
+  unsigned int minBottomIndex = ((fb->width * fb->height) - (cropBottom * fb->width)) * 2;
+  unsigned short maxX = fb->width - cropRight; // In pixels
+  unsigned short newWidth = fb->width - cropLeft - cropRight;
+  unsigned short newHeight = fb->height - cropTop - cropBottom;
+  size_t newJpgSize = newWidth * newHeight * 2;
 
-    if (Serial.available() > 0)
+  unsigned int writeIndex = 0;
+  // Loop over all bytes
+  for (int i = 0; i < fb->len; i += 2)
+  {
+    // Calculate current X, Y pixel position
+    int x = (i / 2) % fb->width;
+
+    // Crop from the top
+    if (i < maxTopIndex)
     {
-        delay(50);
-        Serial.println(Serial.read()); // Read the incoming command until a newline character is encountered
-
-        if (!capture_still())
-        {
-            Serial.println("Failed capture");
-            delay(3000);
-
-            return;
-        }
-
-        // convolve_gaussian();
+      continue;
     }
 
-    // find_max_pix();
-    /*
-    Serial.print("My X: ");
-    Serial.print(max_x);
-    Serial.print("My Y: ");
-    Serial.print(max_y);
+    // Crop from the bottom
+    if (i > minBottomIndex)
+    {
+      continue;
+    }
 
-    Serial.println("=================");
-    */
+    // Crop from the left
+    if (x <= cropLeft)
+    {
+      continue;
+    }
+
+    // Crop from the right
+    if (x > maxX)
+    {
+      continue;
+    }
+
+    // If we get here, keep the pixels
+    fb->buf[writeIndex++] = fb->buf[i];
+    fb->buf[writeIndex++] = fb->buf[i + 1];
+  }
+
+  // Set the new dimensions of the framebuffer for further use.
+  fb->width = newWidth;
+  fb->height = newHeight;
+  fb->len = newJpgSize;
 }
 
-/**
+void loop()
+{
+  // Check for incoming serial commands
+  if (Serial.available() > 0)
+  {
+    delay(50);
+    Serial.read(); // Read the incoming command until a newline character is encountered
+    grabImage();
+    // flush serial
+      while (Serial.available() > 0) {
+      char c = Serial.read();
+  }
 
-*/
+    }
+  
+}
+
 void cameraInit()
 {
 
-    camera_config_t config;
-    config.ledc_channel = LEDC_CHANNEL_0;
-    config.ledc_timer = LEDC_TIMER_0;
-    config.pin_d0 = Y2_GPIO_NUM;
-    config.pin_d1 = Y3_GPIO_NUM;
-    config.pin_d2 = Y4_GPIO_NUM;
-    config.pin_d3 = Y5_GPIO_NUM;
-    config.pin_d4 = Y6_GPIO_NUM;
-    config.pin_d5 = Y7_GPIO_NUM;
-    config.pin_d6 = Y8_GPIO_NUM;
-    config.pin_d7 = Y9_GPIO_NUM;
-    config.pin_xclk = XCLK_GPIO_NUM;
-    config.pin_pclk = PCLK_GPIO_NUM;
-    config.pin_vsync = VSYNC_GPIO_NUM;
-    config.pin_href = HREF_GPIO_NUM;
-    config.pin_sscb_sda = SIOD_GPIO_NUM;
-    config.pin_sscb_scl = SIOC_GPIO_NUM;
-    config.pin_pwdn = PWDN_GPIO_NUM;
-    config.pin_reset = RESET_GPIO_NUM;
-    config.xclk_freq_hz = 20000000;
-    // config.frame_size = FRAMESIZE_QVGA;
-    // config.pixel_format = PIXFORMAT_JPEG; // for streaming
-    // config.pixel_format = PIXFORMAT_RGB565; // for face detection/recognition
-    config.grab_mode = CAMERA_GRAB_WHEN_EMPTY;
-    config.fb_location = CAMERA_FB_IN_PSRAM;
-    config.jpeg_quality = 12;
-    config.pixel_format = PIXFORMAT_GRAYSCALE;
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  // config.frame_size = FRAMESIZE_QVGA;
+  // config.pixel_format = PIXFORMAT_JPEG; // for streaming
+  // config.pixel_format = PIXFORMAT_RGB565; // for face detection/recognition
+  config.grab_mode = CAMERA_GRAB_WHEN_EMPTY;
+  config.fb_location = CAMERA_FB_IN_PSRAM;
+  config.jpeg_quality = 12;
+  if (isCROP)
+  {
+    config.pixel_format = PIXFORMAT_RGB565;
+    config.frame_size = FRAMESIZE_SXGA;
+    config.fb_count = 2;
+  }
+  else
+  {
+    config.pixel_format = PIXFORMAT_JPEG;
     config.frame_size = FRAMESIZE_QVGA; // for streaming}
 
     config.fb_count = 1;
-
-    esp_err_t err = esp_camera_init(&config);
-    if (err != ESP_OK)
-    {
-        Serial.println("Camera init failed with error: " + err);
-        return;
-    }
-
-    sensor_t *s = esp_camera_sensor_get();
-    s->set_hmirror(s, 1);
-    s->set_vflip(s, 1);
-    s->set_special_effect(s, 2); // 0 to 6 (0 - No Effect, 1 - Negative, 2 - Grayscale, 3 - Red Tint, 4 - Green Tint, 5 - Blue Tint, 6 - Sepia)
-}
-
-/**
-   Capture image and do down-sampling
-*/
-bool capture_still()
-{
-    camera_fb_t *frame_buffer = esp_camera_fb_get();
-
-    if (!frame_buffer)
-        return false;
-
-    // set all 0s in current frame
-    for (int y = 0; y < H; y++)
-        for (int x = 0; x < W; x++)
-            current_frame[y][x] = 0;
-
-    // down-sample image in blocks
-    for (uint32_t i = 0; i < WIDTH * HEIGHT; i++)
-    {
-        const uint16_t x = i % WIDTH;
-        const uint16_t y = floor(i / WIDTH);
-        const uint8_t block_x = floor(x / BLOCK_SIZE);
-        const uint8_t block_y = floor(y / BLOCK_SIZE);
-        const uint8_t pixel = frame_buffer->buf[i];
-        const uint16_t current = current_frame[block_y][block_x];
-
-        // average pixels in block (accumulate)
-        current_frame[block_y][block_x] += pixel;
-    }
-
-    // average pixels in block (rescale)
-    for (int y = 0; y < H; y++)
-        for (int x = 0; x < W; x++)
-            current_frame[y][x] /= BLOCK_SIZE * BLOCK_SIZE;
-
-#if 1
-    // Convert the 2D array to a byte array
-    uint8_t* byte_array = (uint8_t*)current_frame;
-    size_t byte_array_length = H * W * sizeof(uint16_t);
-
-    // Send the byte array over serial
-    Serial.print("+++");
-    Serial.write(byte_array, byte_array_length);
-    Serial.print("---");
-
-/*
-    Serial.println("+++");
-    print_frame(current_frame);
-    Serial.println("---");
-    */
-#endif
-
-    esp_camera_fb_return(frame_buffer);
-    return true;
-}
-
-//  Different kernels:
-// http://blog.dzl.dk/2019/06/08/compact-gaussian-interpolation-for-small-displays/
-// Sigma=1
-#define P0 (0.077847)
-#define P1 (0.123317 + 0.077847)
-#define P2 (0.195346 + 0.123317 + 0.123317 + 0.077847)
-/*
-  //Sigma=0.5
-  #define P0 (0.024879)
-  #define P1 (0.107973+0.024879)
-  #define P2 (0.468592+0.107973+0.107973+0.024879)
-*/
-/*
-  //Sigma=2
-  #define P0 (0.102059)
-  #define P1 (0.115349+0.102059)
-  #define P2 (0.130371+0.115349+0.115349+0.102059)
-*/
-
-const float kernel[4][4] =
-    {
-        {P0, P1, P1, P2},
-        {P1, P0, P2, P1},
-        {P1, P2, P0, P1},
-        {P2, P1, P1, P0}};
-
-/*
-   Convolve the image with a gaussian and find the maximum position of the laserbeam
-   Inspired by: http://www.songho.ca/dsp/convolution/convolution.html
-*/
-void convolve_gaussian()
-{
-    // find center position of kernel (half of kernel size)
-    int kCols = 4; // size kernel x
-    int kRows = 4; // size kernel y
-    int kCenterX = kCols / 2;
-    int kCenterY = kRows / 2;
-
-    for (int y = 0; y < H; y++) // rows
-    {
-        for (int x = 0; x < W; x++) // columns
-        {
-            proc_frame[y][x] = 0;           // Initiliaze with zero
-            for (int m = 0; m < kRows; ++m) // kernel rows
-            {
-                int mm = kRows - 1 - m;         // row index of flipped kernel
-                for (int n = 0; n < kCols; ++n) // kernel columns
-                {
-                    int nn = kCols - 1 - n; // column index of flipped kernel
-
-                    // index of input signal, used for checking boundary
-                    int yy = y + (kCenterY - mm);
-                    int xx = x + (kCenterX - nn);
-
-                    // ignore input samples which are out of bound
-                    if (yy >= 0 && yy < H && xx >= 0 && xx < W)
-                        proc_frame[y][x] += current_frame[yy][xx] * kernel[mm][nn];
-                }
-            }
-        }
-    }
-
-#if 0
-    // Convert the 2D array to a byte array
-    uint8_t* byte_array = (uint8_t*)proc_frame;
-    size_t byte_array_length = H * W * sizeof(uint16_t);
-
-    // Send the byte array over serial
-    Serial.print("+++");
-    Serial.write(byte_array, byte_array_length);
-    Serial.print("---");
-    // flush serial
-      while (Serial.available() > 0) {
-    char c = Serial.read();
   }
-    /*
-    
-    Serial.println("+++");
-    print_frame(proc_frame);
-    Serial.println("---");
-    */
-#endif
+
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK)
+  {
+    return;
+  }
+
+  sensor_t *s = esp_camera_sensor_get();
+  s->set_hmirror(s, 1);
+  s->set_vflip(s, 1);
 }
-
-/**
-   Copy current frame to previous
-*/
-void find_max_pix()
+void grabImage()
 {
-    int mymax = 0;
 
-    for (int y = 0; y < H; y++)
+  camera_fb_t *fb = esp_camera_fb_get();
+  if (isCROP)
+  {
+
+    // Crop image (frame buffer, cropLeft, cropRight, cropTop, cropBottom)
+    unsigned short cropLeft = x - Nroi / 2;
+    unsigned short cropRight = x + Nroi / 2;
+    unsigned short cropTop = y - Nroi / 2;
+    unsigned short cropBottom = y + Nroi / 2;
+
+    crop_image(fb, 550, 450, 100, 190);
+    // crop_image(fb, cropLeft, cropRight, cropTop, cropBottom);
+    //  Create a buffer for the JPG in psram
+    uint8_t *jpg_buf = (uint8_t *)heap_caps_malloc(200000, MALLOC_CAP_SPIRAM | MALLOC_CAP_8BIT);
+
+    if (jpg_buf == NULL)
     {
-        for (int x = 0; x < W; x++)
-        {
-            if (proc_frame[y][x] > mymax)
-            {
-                mymax = proc_frame[y][x];
-                max_x = x;
-                max_y = y;
-            }
-        }
+      printf("Malloc failed to allocate buffer for JPG.\n");
     }
-}
-
-/**
-   For serial debugging
-   @param frame
-*/
-void print_frame(uint16_t frame[H][W])
-{
-    for (int y = 0; y < H; y++)
+    else
     {
-        for (int x = 0; x < W; x++)
-        {
-            int myval = 0;
-            if (frame[y][x] > 0)
-                myval = frame[y][x];
-            Serial.print(myval);
-            Serial.print(',');
-        }
+      size_t jpg_size = 0;
 
-        Serial.println();
+      // Convert the RAW image into JPG
+      // The parameter "31" is the JPG quality. Higher is better.
+      fmt2jpg(fb->buf, fb->len, fb->width, fb->height, fb->format, 31, &jpg_buf, &jpg_size);
+      printf("Converted JPG size: %d bytes \n", jpg_size);
+      String encoded = base64::encode(jpg_buf, jpg_size);
+      Serial.write(encoded.c_str(), encoded.length());
+      Serial.println();
     }
+  }
+  else
+  {
+
+    if (!fb || fb->format != PIXFORMAT_JPEG)
+    {
+      Serial.println("Failed to capture image");
+    }
+    else
+    {
+      delay(40);
+
+      String encoded = base64::encode(fb->buf, fb->len);
+      Serial.write(encoded.c_str(), encoded.length());
+      Serial.println();
+    }
+
+    esp_camera_fb_return(fb);
+  }
 }
