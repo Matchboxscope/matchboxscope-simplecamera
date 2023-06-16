b61320f0fecbadd01612bd2ff10b52b29a9802ea
beniroquai <bene.d@gmx.de>
2023-06-16 08:05:29 +0200

    Update app_httpd.cpp

--- main/app_httpd.cpp
+++ main/app_httpd.cpp
@@ -420,7 +420,7 @@ static esp_err_t stream_handler(httpd_req_t *req)
         setLamp(0);
     Serial.println("Stream ended");
     last_frame = 0;
-    F_RDLCK = false; 
+    IS_STREAMING = false; 
     return res;
 }
 
